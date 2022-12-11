7900a159e2897a14fe7fc029c8a58d682ba5a605
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-11 11:40:28 +0530

    Another merged pdf added

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const PDFMerger = require('pdf-merger-js');
 var merger = new PDFMerger();
 
 (async () => {
-  await merger.add('NFA.pdf');  
-  await merger.add('Modern round ciphers.pdf'); 
-  await merger.save('merged.pdf'); 
+  //await merger.add('NFA.pdf');  
+  //await merger.add('Modern round ciphers.pdf');
+  await merger.add('Simple Modern method of cryptography.pdf');  
+  await merger.add('Traditional Method of cryptography.pdf');  
+  await merger.save('output.pdf'); 
 })();
